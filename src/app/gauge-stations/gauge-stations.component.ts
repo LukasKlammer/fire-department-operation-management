359cbb4606688afdb75cc88bbba3e8ad1d8c2b08
unknown <klammer.lukas@hotmail.com>
2022-09-25 09:23:43 +0200

    hide gauge stations because of problem with different origin policy

--- src/app/gauge-stations/gauge-stations.component.ts
+++ src/app/gauge-stations/gauge-stations.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-gauge-stations',
@@ -7,9 +6,9 @@ import { FormControl } from '@angular/forms';
   styleUrls: ['./gauge-stations.component.scss']
 })
 export class GaugeStationsComponent implements OnInit {
-  url: string = 'http://daten.buergernetz.bz.it/services/weather/station?categoryId=2&lang=de&format=json'
+  // url: string = 'https://daten.buergernetz.bz.it/services/weather/station?categoryId=2&lang=de&format=json'
   allStations: any[] = [];
-  initialStationsNames: string[] = ['AHR BEI ST.GEORGEN', 'RIENZ BEI STEGEN']
+  initialStationsNames: string[] = ['AHR BEI ST.GEORGEN', 'RIENZ BEI STEGEN'];
   selectedStationsNames: string[] = [];
   selectedStationsData: any[] = [];
   interval: any;
@@ -19,120 +18,120 @@ export class GaugeStationsComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    this.initData();
-    this.startPeriodicReloading();
+    // this.initData();
+    // this.startPeriodicReloading();
   }
 
-  ngOnDestroy(): void {
-    clearInterval(this.interval);
-  }
+  // ngOnDestroy(): void {
+  //   clearInterval(this.interval);
+  // }
 
-  startPeriodicReloading() { // to re-fetch data every x minutes
-    this.interval = setInterval(() => {
-      this.initData();
-    }, 300000)
-  }
+  // startPeriodicReloading() { // to re-fetch data every x minutes
+  //   this.interval = setInterval(() => {
+  //     this.initData();
+  //   }, 300000)
+  // }
 
-  async initData() {
-    await this.fetchStationsData();
-    this.prepareData();
-    this.initSelectedStations();
-    this.calculateFlowBruneck();
-    this.addAlertThresholds();
-  }
+  // async initData() {
+  //   await this.fetchStationsData();
+  //   this.prepareData();
+  //   this.initSelectedStations();
+  //   this.calculateFlowBruneck();
+  //   this.addAlertThresholds();
+  // }
 
-  async fetchStationsData() {
-    try {
-      let response = await fetch(this.url);
-      let responseAsJson = await response.json();
-      this.allStations = this.sortStations(responseAsJson.rows);
-    } catch (e) {
-      console.error('error while loading resource: ' + e);
-    }
-  }
+  // async fetchStationsData() {
+  //   try {
+  //     let response = await fetch(this.url);
+  //     let responseAsJson = await response.json();
+  //     this.allStations = this.sortStations(responseAsJson.rows);
+  //   } catch (e) {
+  //     console.error('error while loading resource: ' + e);
+  //   }
+  // }
 
-  prepareData() {
-    this.convertGeoLocation();
-    this.roundFlow();
-  }
+  // prepareData() {
+  //   this.convertGeoLocation();
+  //   this.roundFlow();
+  // }
 
-  sortStations(stations: any[]) {
-    return stations.sort((a, b) => {
-      if (a.name < b.name)
-        return -1;
-      if (a.name > b.name)
-        return 1;
-      return 0;
-    });
-  }
+  // sortStations(stations: any[]) {
+  //   return stations.sort((a, b) => {
+  //     if (a.name < b.name)
+  //       return -1;
+  //     if (a.name > b.name)
+  //       return 1;
+  //     return 0;
+  //   });
+  // }
 
-  roundFlow() {
-    this.allStations.map((station) => {
-      station.q = Math.round(station.q);
-    })
-  }
+  // roundFlow() {
+  //   this.allStations.map((station) => {
+  //     station.q = Math.round(station.q);
+  //   })
+  // }
 
-  calculateFlowBruneck() {
-    let riverRienz = this.getStationByName('RIENZ BEI STEGEN');
-    let riverAhr = this.getStationByName('AHR BEI ST.GEORGEN');
-    this.flowLevelBruneck = riverRienz.q - riverAhr.q;
-  }
+  // calculateFlowBruneck() {
+  //   let riverRienz = this.getStationByName('RIENZ BEI STEGEN');
+  //   let riverAhr = this.getStationByName('AHR BEI ST.GEORGEN');
+  //   this.flowLevelBruneck = riverRienz.q - riverAhr.q;
+  // }
 
-  getStationByName(name: string) {
-    return this.allStations.find((item) => {
-      return item.name == name;
-    })
-  }
+  // getStationByName(name: string) {
+  //   return this.allStations.find((item) => {
+  //     return item.name == name;
+  //   })
+  // }
 
-  getSelectedStations() {
-    this.selectedStationsData = this.allStations.filter((station) => {
-      return this.selectedStationsNames.includes(station.name);
-    })
-  }
+  // getSelectedStations() {
+  //   this.selectedStationsData = this.allStations.filter((station) => {
+  //     return this.selectedStationsNames.includes(station.name);
+  //   })
+  // }
 
-  checkShowInitButton() {
-    if (this.arraysEqual(this.selectedStationsNames, this.initialStationsNames)) {
-      this.showInitButton = false;
-    } else {
-      this.showInitButton = true;
-    }
-  }
+  // checkShowInitButton() {
+  //   if (this.arraysEqual(this.selectedStationsNames, this.initialStationsNames)) {
+  //     this.showInitButton = false;
+  //   } else {
+  //     this.showInitButton = true;
+  //   }
+  // }
 
-  arraysEqual(a: string[], b: string[]) {
-    if (a === b) return true;
-    if (a == null || b == null) return false;
-    if (a.length !== b.length) return false;
-    // If you don't care about the order of the elements inside the array, you should sort both arrays here.
-    for (var i = 0; i < a.length; ++i) {
-      if (a[i] !== b[i]) return false;
-    }
-    return true;
-  }
+  // arraysEqual(a: string[], b: string[]) {
+  //   if (a === b) return true;
+  //   if (a == null || b == null) return false;
+  //   if (a.length !== b.length) return false;
+  //   // If you don't care about the order of the elements inside the array, you should sort both arrays here.
+  //   for (var i = 0; i < a.length; ++i) {
+  //     if (a[i] !== b[i]) return false;
+  //   }
+  //   return true;
+  // }
 
-  addAlertThresholds() {
-    for (let i = 0; i < this.allStations.length; i++) {
-      let station = this.allStations[i];
-      if (station.name == 'RIENZ BEI STEGEN') {
-        station['preAlertThreshold'] = 300;
-        station['alertThreshold'] = 350;
-      }
-      if (station.name == 'AHR BEI ST.GEORGEN') {
-        station['preAlertThreshold'] = 260;
-        station['alertThreshold'] = 290;
-      }
-    }
-  }
+  // addAlertThresholds() {
+  //   for (let i = 0; i < this.allStations.length; i++) {
+  //     let station = this.allStations[i];
+  //     if (station.name == 'RIENZ BEI STEGEN') {
+  //       station['preAlertThreshold'] = 300;
+  //       station['alertThreshold'] = 350;
+  //     }
+  //     if (station.name == 'AHR BEI ST.GEORGEN') {
+  //       station['preAlertThreshold'] = 260;
+  //       station['alertThreshold'] = 290;
+  //     }
+  //   }
+  // }
 
-  initSelectedStations() {
-    this.selectedStationsNames = this.initialStationsNames;
-    this.getSelectedStations();
-    this.showInitButton = false;
-  }
+  // initSelectedStations() {
+  //   this.selectedStationsNames = this.initialStationsNames;
+  //   this.getSelectedStations();
+  //   this.showInitButton = false;
+  // }
 
-  convertGeoLocation() {
-    this.allStations.map((station) => {
-      station.latitude = station.latitude.replace(',', '.');
-      station.longitude = station.longitude.replace(',', '.');
-    })
-  }
+  // convertGeoLocation() {
+  //   this.allStations.map((station) => {
+  //     station.latitude = station.latitude.replace(',', '.');
+  //     station.longitude = station.longitude.replace(',', '.');
+  //   })
+  // }
 }
